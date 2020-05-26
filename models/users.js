bf5e95bcb232d7e4864c0461e3f4192f10a5d5d9
VenkatPantham <venkatpantham5568@gmail.com>
2020-05-26 21:03:33 +0530

    Passport

--- models/users.js
+++ models/users.js
@@ -1,21 +1,15 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+var passportLocalMangoose = require("passport-local-mongoose");
 
 var userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
   admin: {
     type: Boolean,
     default: false,
   },
 });
+
+userSchema.plugin(passportLocalMangoose);
 
 var Users = mongoose.model("User", userSchema);
 
